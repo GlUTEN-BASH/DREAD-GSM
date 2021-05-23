15f865f88df7b8d81165c66e8d1aa1b7c73a7ac3
GLUTEN <57950551+GlUTEN-BASH@users.noreply.github.com>
2021-05-23 20:17:03 +0300

    Add files via upload

---
+++ dreadgsm.py
@@ -0,0 +1,94 @@
+import os, colorama
+from colorama import init, Fore, Back
+def clear():
+     os.system('clear')
+clear()
+
+print(f"""
+{Back.WHITE}
+{Fore.BLACK}
+ ____  ____  _____    _    ____     ____ ____  __  __ 
+|  _ \|  _ \| ____|  / \  |  _ \   / ___/ ___||  \/  |
+| | | | |_) |  _|   / _ \ | | | | | |  _\___ \| |\/| |
+| |_| |  _ <| |___ / ___ \| |_| | | |_| |___) | |  | |
+|____/|_| \_\_____/_/   \_\____/   \____|____/|_|  |_|
+                                                      
+{Fore.RESET}
+{Back.RESET}
+""")
+def select():
+    print(f"""
+    {Fore.GREEN}
+    By GLUTEN
+    Inspired by fuckwbored
+    {Fore.BLUE}
+=============================
+    Select:
+
+    Info - 0
+
+    Setup DRE@AD - 1
+
+    Run DRE@AD attack menu - 2
+=============================
+
+{Fore.WHITE}
+    """)
+    global ss
+    ss = input(f"Dre@d{Fore.GREEN}~{Fore.BLUE}: ")
+
+
+def main():
+    select()
+    if ss == '0':
+        clear()
+        print(f"""
+              {Fore.RED}
+Made by GLUTEN,
+a 12 y/o boy 
+
+Inspired by fuckwbored
+admin of TG:termuxqew
+
+DRE@D AUTOGSM V. 1.2
+
+Script made for automatisation
+of using IMSI-catcher and searching
+base stations.
+              """)
+        print(f"{Fore.BLUE} Back? Y/N {Fore.WHITE}")
+
+        sw = input(f"Dre@d{Fore.GREEN}~/info{Fore.BLUE}:")
+        if sw == 'Y':
+            clear()
+            main()
+        else:
+            clear()
+            print(f"{Fore.RED} Thanks for using!")
+            os.system('exit')
+    elif ss == '1':
+        setup()
+    elif ss == '2':
+        os.system('python main.py')
+
+def setup():
+    print(f"{Fore.GREEN} Installing...{Fore.WHITE}")
+    os.system('apt update')
+    os.system('apt -y install git && apt -y install python3 && git clone https://github.com/Oros42/IMSI-catcher.git')
+    os.system('apt -y install gnuradio gr-gsm gnuradio-dev git cmake autoconf libtool pkg-config g++ gcc make libc6 libc6-dev libcppunit-dev swig doxygen liblog4cpp5v5 liblog4cpp5-dev gr-osmosdr osmo-sdr libosmosdr-dev libboost-all-dev libgmp-dev liborc-dev libboost-regex-dev build-essential automake librtlsdr-dev libfftw3-dev tshark')
+    os.system('apt -y install gqrx-sdr:i386 gqrx-sdr libcppunit-dev libcppunit-doc')
+    os.system('apt -y install libc6 libgcc1')
+    os.system('apt -y install libcppunit-1.13-0v5 libcunit1 libcunit1-doc libcunit1-dev libjs-qunit libjs-sink-test node-sink-test')
+    os.system('apt -y install osinfo-db-tools iraf-dev libgnuradio-osmosdr0.2.0:i386')
+    print(f"{Fore.GREEN} Done! Now you can run main.py or run in menu{Fore.WHITE}")
+    print(f"{Fore.BLUE} Exit to menu? Y/N {Fore.WHITE}")
+    sw = input(f"Dre@d{Fore.GREEN}~{Fore.BLUE}/setup:")
+    if sw == 'Y':
+        clear()
+        main()
+    else:
+        clear()
+        print(f"{Fore.RED} Thanks for using!")
+        os.system('exit')
+
+main()
---
+++ main.py
@@ -0,0 +1,106 @@
+import os, colorama, subprocess, time
+from multiprocessing import Process
+from colorama import init, Fore, Back
+def clear():
+     os.system('clear')
+clear()
+print(f"""
+{Back.WHITE}
+{Fore.BLACK}
+ ____  ____  _____    _    ____     ____ ____  __  __ 
+|  _ \|  _ \| ____|  / \  |  _ \   / ___/ ___||  \/  |
+| | | | |_) |  _|   / _ \ | | | | | |  _\___ \| |\/| |
+| |_| |  _ <| |___ / ___ \| |_| | | |_| |___) | |  | |
+|____/|_| \_\_____/_/   \_\____/   \____|____/|_|  |_|
+                                                  s    
+{Fore.RESET}
+{Back.RESET}
+""")
+print(f"{Fore.GREEN}Attack menu")
+
+def livemon(arg1, arg2):
+    os.system(f'grgsm_livemon -f {arg2} --args={arg1} >/dev/null 2>&1')
+
+def listen(arg1, arg2):
+    clear()
+    print(f"{Fore.BLUE} Starting listen with {arg1} on frequency {arg2}... {Fore.GREEN}")
+    p = Process(target=livemon, args=(arg1, arg2,))
+    p.start()
+    os.chdir('IMSI-catcher')
+    time.sleep(2)
+    os.system('clear')
+    print(f"{Fore.BLUE}Listening with {arg1} on frequency {arg2}... {Fore.GREEN}")
+    os.system('python3 simple_IMSI-catcher.py -s')
+
+def main(ss):
+    global sdr
+    if ss == '0':
+        sdr = 'rtl'
+    if ss == '1':
+        sdr = 'bladerf'
+    if ss == '2':
+        sdr = 'hackrf'
+    if ss == '3':
+        sdr = 'airspy'
+    if ss == '4':
+        sdr = 'airspyf'
+    if ss == '5':
+        sdr = 'soapy'
+    
+    clear()
+    print(f"{Fore.GREEN} Your sdr is: {sdr}, right? Y/N {Fore.WHITE}")
+    sr = input(f"Dre@d{Fore.GREEN}~/attack/sdr/{Fore.BLUE}: ")
+    if sr == 'Y':
+        scan(sdr)
+    if sr == 'N':
+        clear()
+        select()
+
+def select():
+    print(f"""
+    {Fore.BLUE}
+=============================
+    Select your SDR:
+
+    rtl-sdr - 0
+
+    bladerf - 1
+
+    hackrf - 2
+
+    airspy - 3
+
+    airspyf - 4
+
+    soapysdr - 5
+=============================
+
+{Fore.WHITE}
+    """)
+    ss = input(f"Dre@d{Fore.GREEN}~/attack/sdr{Fore.BLUE}: ")
+    main(ss)
+
+def scan(arg):
+    print(f"{Fore.BLUE} Scanning with {arg}... {Fore.GREEN}")
+    os.system(f'grgsm_scanner -v -b GSM900 --args={arg}')
+    print(f"{Fore.BLUE}Done!{Fore.WHITE}")
+    starter()
+
+
+def starter():
+    print(f"{Fore.GREEN}Type in your preferred frequency:{Fore.WHITE}")
+    sr = input(f"Dre@d{Fore.GREEN}~/attack/sdr/freq{Fore.BLUE}: ")
+    print(f"{Fore.GREEN} Your frequency is: {sr}, right? Y/N or redoscan {Fore.WHITE}")
+    sd = input(f"Dre@d{Fore.GREEN}~/attack/sdr/freq{Fore.BLUE}: ")
+    if sd == 'Y':
+        clear()
+        listen(sdr, sr)
+    elif sd == 'redoscan':
+        clear()
+        scan(sdr)
+    else:
+        starter()
+        
+
+select()
+
